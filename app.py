f59cbed8382a91f0e953b8d7808303bd8dd781b2
Aadi Sharma <as6311254@gmail.com>
2026-07-10 13:42:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ MODEL_FILE = "Customer_Prediction_Model.pkl"
 
 if not os.path.exists(MODEL_FILE):
     url = "https://drive.google.com/file/d/1XIFORRSiw1S0hMpimNtxgD7qbEW3WUgF"
-    gdown.download(url, MODEL_FILE, quiet=False)
+   gdown.download(url, MODEL_FILE, quiet=False, fuzzy=True)
 
 model = joblib.load(MODEL_FILE)
 feature_columns = [
